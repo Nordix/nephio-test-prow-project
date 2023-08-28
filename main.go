8ab23d35776e421e22ff541f92d75e1bf5c8c909
Akash Rajguru <akash.rajguru@est.tech>
2023-08-28 18:50:29 +0100

    test postsubmit job 7

--- main.go
+++ main.go
@@ -16,4 +16,5 @@ func main() {
 	fmt.Println("One plus two seems to be:", a)
 	fmt.Println("One plus two seems to be:", a)
 	fmt.Println("One plus two seems to be:", a)
+	fmt.Println("One plus two seems to be:", a)
 }
